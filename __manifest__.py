649de175967be12a85495a7c7eeb54d92ee4b827
Mykola Ostroukh <koluchiy.nick@gmail.com>
2022-07-19 14:08:49 +0000

    start

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Print form Invoice for Ukraine',
+    'summary': 'Print forms Rahunok and Akt in Invoice for Ukraine',
+    'author': 'Mikola Ostroukh',
+    'license': "LGPL-3",
+    'category': 'Accounting',
+    'version': '15.0.1.0.0',
+    'depends': ['base','account'],
+    'data': [
+        'views/res_partner_views.xml',
+        'views/res_currency_views.xml',
+        'report/invoice_report_views.xml',
+        'report/invoice_report_rah_templates.xml',
+        'report/invoice_report_akt_templates.xml',
+    ],
+}
