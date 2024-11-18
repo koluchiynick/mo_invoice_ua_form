5d0fa1a1eba1b5a0c878335f4473e1642af830f9
Mykola Ostroukh <koluchiy.nick@gmail.com>
2024-11-18 15:27:21 +0200

    edits for correct operation in version 18

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     'author': 'Mikola Ostroukh',
     'license': "LGPL-3",
     'category': 'Accounting',
-    'version': '15.0.1.0.0',
+    'version': '18.0.1.0.0',
     'depends': ['base', 'account'],
     'data': [
         'views/res_company_views.xml',
